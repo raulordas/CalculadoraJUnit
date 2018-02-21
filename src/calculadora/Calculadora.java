884189b7500586e95944787a00553d5f0ba12031
raulordas <ordasraul@gmail.com>
2018-02-21 11:34:37 +0100

    Cambios

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -14,14 +14,30 @@ public class Calculadora {
 	}
 	
 	public int rest() {
-		return num1 - num2;
+		if (num1EsMayor()) return num1 - num2;
+		else return num2 -num1;
 	}
-	
+	public boolean num1EsMayor() {
+		if(num1 > num2) return true;
+		else return false;
+	}
 	public int multiplicacion() {
 		return num1 * num2;
 	}
 	
 	public int division() {
 		return num1 / num2;
+	}
+	public Integer division2() {
+		if (num2 == 0) {
+			return null;
+		} else { 
+			return num1/num2;
+		}
+	}
+	public int division3() {
+		if (num2 == 0) throw new java.lang.ArithmeticException("División por cero");
+		else
+			return num1 / num2;
 	}
 }
--- src/calculadora/CalculadoraTest.java
+++ src/calculadora/CalculadoraTest.java
@@ -2,47 +2,80 @@ package calculadora;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculadoraTest {
-	Calculadora calcu = new Calculadora(20, 30);
+	Calculadora calcu = null;
+	
+	@Before
+	public void creaCalculadora() {
+		calcu = new Calculadora (20, 30);
+	}
+	
+	@After
+	public void borraCalculadora() {
+		calcu = null;
+	}
+	
 	@Test
 	public void testSuma() {
-		Calculadora calcu = new Calculadora(20, 30);
 		int resultado = calcu.suma();
 		assertEquals(50, resultado);
 	}
 
 	@Test
 	public void testRest() {
-		Calculadora calcu = new Calculadora(20, 30);
 		int resultado = calcu.rest();
-		assertEquals(-10, resultado);
+		assertEquals(10, resultado);
 	}
 
 	@Test
 	public void testMultiplicacion() {
-		Calculadora calcu = new Calculadora(20, 30);
 		int resultado = calcu.multiplicacion();
 		assertEquals("Fallo en la multilpicación" ,600, resultado);
 	}
 
 	@Test
 	public void testDivision() {
-		Calculadora calcu = new Calculadora(8, 2);
+		//Calculadora calcu = new Calculadora(8, 2);
 		int resultado = calcu.division();
-		assertEquals(4, resultado);
+		assertEquals(0, resultado);
 	}
 	
 	@Test
 	public void testDivisionPorCero() {
-		Calculadora calcu = new Calculadora(2, 0);
-		
+		//Calculadora calcu = new Calculadora(2, 0);
 		try {
 			int resultado = calcu.division();
-			fail ("FALLO, debería haber lanzado la excepcion");
+			
 		} catch (ArithmeticException excp) {
+			fail ("FALLO, debería haber lanzado la excepcion");
 		//PRUEBA SATISFACTORIA	
 		}
 	}
+	
+	@Test
+	public void num1EsMayor() {
+		Calculadora calcu = new Calculadora(20, 30);
+		boolean resultado = calcu.num1EsMayor();
+		//assertEquals(false, resultado)
+		//assertTrue("No es true", resultado); -- Con esto fallaría la prueba porque debería ser false con los datos
+		assertFalse("No es true", resultado);
+	}
+	
+	@Test
+	public void divisionPorCero2() {
+		//Calculadora calcu = new Calculadora(2, 0);
+		Integer resultado = calcu.division2();
+		assertNotNull(resultado);
+	}
+	
+	/*@Test(expected=java.lang.ArithmeticException.class)
+	public void divisionPorCero3() {
+		//Calculadora calcu = new Calculadora (2, 0);
+		int resultado = calcu.division3();	
+	}
+	*/
 }
